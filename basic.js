e8df0e73e34da996d2637150a097b48517a7d1e4
Farzana Eva <farzanaeva@Farzanas-iMac.local>
2023-02-27 23:56:54 -0500

    basic js file added

---
+++ basic.js
@@ -0,0 +1,16 @@
+let myName = "Farzana";
+console.log(myName);
+
+myName = "Eva";
+console.log(myName);
+// Normal function
+//function printMyName(name) {
+// console.log(name);
+//}
+//printMyName("Farzana Eva");
+
+// Arrow function
+const printMyName = (name) => {
+  console.log(name);
+};
+printMyName("Maria Islam");
